eb770479470d0fd4f067f0e6447555dc7a24aed2
Dean Bloomingdale <dean.bloomingdale1@gmail.com>
2024-06-21 21:20:50 -0400

    writeToFile function and init

--- index.js
+++ index.js
@@ -32,25 +32,43 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'test',
+        name: 'tests',
         message: "Provide instructions for testing the application.",
     },
     {
-
+        type: 'list',
+        name: 'license',
+        message: "Which license does this project use?",
+        choices: ['MIT', 'GPLv2', 'GPLv3', 'LGPLv2.1', 'LGPLv3', 'Apache 2.0', 'MPL 2.0', 'BSD', 'Creative Commons', 'None']
     },
     {
-
+        type: 'input',
+        name: 'github',
+        message: "Enter your GitHub username.",
     },
     {
-
+        type: 'input',
+        name: 'email',
+        message: "Enter your e-mail address.",
     },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('README.md file has been successfully generated!');
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        writeToFile("./Generated_README/README.md", generateMarkdown(data))
+    });
+}
 // Function call to initialize app
 init();
